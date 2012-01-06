5bc46c8875ecfcf3970919b83ac30e059738fee4
Naitik Shah <n@daaku.org>
2012-01-07 06:26:04 +1300

    tests for extracting :css from multiple elements

--- test/dom_css/test/core.clj
+++ test/dom_css/test/core.clj
@@ -5,9 +5,19 @@
     [dom-css.core :only [inline extract]]
     [clojure.test :only [deftest testing is]]))
 
-(deftest extract-test
+(deftest extract-test-single
   (let [input [[:a {:css :foo}]]
         [css [[tag attrs content]]] (extract input)]
     (is (= css [:foo]))
     (is (= "a" tag))
     (is (= nil (:css attrs)))))
+
+(deftest extract-test-multiple
+  (let [input [[:a {:css :foo}] [:b {:css :bar}]]
+        [css [[tag attrs content]]] (extract input)]
+    (is (= css [:foo :bar]))))
+
+(deftest extract-test-multiple-duplicate
+  (let [input [[:a {:css :foo}] [:b {:css :foo}]]
+        [css [[tag attrs content]]] (extract input)]
+    (is (= css [:foo :foo]))))
